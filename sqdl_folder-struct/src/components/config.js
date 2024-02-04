56f116983b6c957d7e3586d33489c4ee0fd50566
kishan kathare <kishankathare45@gmail.com>
2024-02-04 16:02:39 +0530

    changes

--- sqdl_folder-struct/src/components/config.js
+++ sqdl_folder-struct/src/components/config.js
@@ -1,5 +1,3 @@
 export const COOKIE_KEY = "ehbferuifbeifhiue",
-  // GLOBAL_URL = "http://3.110.169.27:5000/api/v1/",
-  // SOCKET_URL = "ws://3.110.169.27:5000";
-  GLOBAL_URL = "http://localhost:5000/api/v1/",
-  SOCKET_URL = "ws://localhost:5000";
+  GLOBAL_URL = "http://3.110.169.27:5000/api/v1/",
+  SOCKET_URL = "ws://3.110.169.27:5000";
