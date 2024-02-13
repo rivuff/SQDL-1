b2e6f5a793775bb7b7c26c6628bbf7390b3de2ee
kishan kathare <kishankathare45@gmail.com>
2024-02-13 21:00:46 +0530

    Changes

--- sqdl_folder-struct/src/components/config.js
+++ sqdl_folder-struct/src/components/config.js
@@ -1,5 +1,3 @@
 export const COOKIE_KEY = "ehbferuifbeifhiue",
-  // GLOBAL_URL = "http://mindeate.com/api/v1/",
-  // SOCKET_URL = "ws://mindeate.com:5000";
-  GLOBAL_URL = "http://localhost:5000/api/v1/",
-  SOCKET_URL = "ws://localhost:5000";
+  GLOBAL_URL = "http://mindeate.com/api/v1/",
+  SOCKET_URL = "ws://mindeate.com:5000";
